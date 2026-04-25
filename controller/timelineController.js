43a2795dedcdf9e0d248726c082f262562590019
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-04-26 02:35:40 +0600

    update

--- controller/timelineController.js
+++ controller/timelineController.js
@@ -8,13 +8,25 @@ import { Timeline } from "../models/timelineSchema.js";
 export const postTimeline = catchAsyncErrors(async (req, res, next) => {
   const { title, description, from, to } = req.body;
 
+  // Validation
+  if (!title || !description || !from) {
+    return next(new ErrorHandler("All fields are required!", 400));
+  }
+
+  // Date validation (optional but good)
+  // if (new Date(from) > new Date(to)) {
+  //   return next(
+  //     new ErrorHandler("'from' date cannot be after 'to' date!", 400),
+  //   );
+  // }
+
   const newTimeline = await Timeline.create({
     title,
     description,
     timeline: { from, to },
   });
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     message: "Timeline Added!",
     newTimeline,
@@ -49,5 +61,64 @@ export const getAllTimelines = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     timelines,
+  });
+});
+
+// ===============
+// Update Timeline
+// ===============
+export const updateTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const { title, description, from, to } = req.body;
+
+  const timeline = await Timeline.findById(id);
+
+  if (!timeline) {
+    return next(new ErrorHandler("Timeline not found!", 404));
+  }
+
+  // Update fields only if provided
+  if (title) timeline.title = title;
+  if (description) timeline.description = description;
+
+  if (from) timeline.timeline.from = from || timeline.timeline.from;
+  if (to !== undefined) timeline.timeline.to = to || timeline.timeline.to;
+
+  await timeline.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Timeline Updated Successfully!",
+    timeline,
+  });
+});
+
+export const updateTimeline1 = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+
+  const updatedTimeline = await Timeline.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        "timeline.from": req.body.from,
+        "timeline.to": req.body.to,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  if (!updatedTimeline) {
+    return next(new ErrorHandler("Timeline not found!", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Timeline updated successfully!",
+    timeline: updatedTimeline,
   });
 });
