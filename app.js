ddceb8569865c7e0b8ef653b77e68578714eaf0b
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-04-26 00:21:43 +0600

    Refactor dotenv config and CORS middleware

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+
 import dbConnection from "./database/dbConnection.js";
 import { errorMiddleware } from "./middlewares/error.js";
+
 import messageRouter from "./router/messageRoutes.js";
 import userRouter from "./router/userRoutes.js";
 import timelineRouter from "./router/timelineRoutes.js";
@@ -13,16 +15,20 @@ import skillRouter from "./router/skillRoutes.js";
 import projectRouter from "./router/projectRoutes.js";
 
 const app = express();
-dotenv.config({ path: "./config/config.env" });
 
+// dotenv.config({ path: "./config/config.env" });
+dotenv.config();
+
+// CORS
 app.use(
   cors({
     origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })
+  }),
 );
 
+// Middlewares
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,9 +37,10 @@ app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-  })
+  }),
 );
 
+// Routes
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/timeline", timelineRouter);
@@ -41,7 +48,29 @@ app.use("/api/v1/softwareapplication", applicationRouter);
 app.use("/api/v1/skill", skillRouter);
 app.use("/api/v1/project", projectRouter);
 
+// DB connection
 dbConnection();
+
+// Error middleware
 app.use(errorMiddleware);
 
 export default app;
+
+/* app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        process.env.PORTFOLIO_URL,
+        process.env.DASHBOARD_URL,
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+*/
