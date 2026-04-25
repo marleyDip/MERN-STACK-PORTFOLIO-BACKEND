a217759cfe6c8f56a575b5e9d8e2f71f8f348c1d
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-04-26 01:42:03 +0600

    update

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,17 +5,9 @@ export const generateToken = (user, message, statusCode, res) => {
     .status(statusCode)
     .cookie("token", token, {
       expires: new Date(
-<<<<<<< HEAD
         Date.now() + Number(process.env.COOKIE_EXPIRES) * 24 * 60 * 60 * 1000,
       ),
       httpOnly: true,
-=======
-        Date.now() + Number(process.env.COOKIE_EXPIRES) * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      sameSite: "None",
-      secure: "true",
->>>>>>> 01408f9db6e9f9b896d80cf40404a295b58e63f8
     })
     .json({
       success: true,
